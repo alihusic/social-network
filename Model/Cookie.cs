3f026cf84ca5f087b928352ee77207cc8f8a8186
Ermin <ermin-lol@hotmail.com>
2016-11-25 18:07:24 +0100

    Add files via upload

---
+++ Model/Cookie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Model
+{
+    public class Cookie
+    {
+        [Key]
+        public int cookieId { get; set; }
+        public int userId { get; set; }
+        public string cookieHash { get; set; }
+
+
+        public virtual User user { get; set; }
+
+    }
+}
