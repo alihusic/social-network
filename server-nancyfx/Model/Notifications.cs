7dae0893254bc687bff486b49fcf1b72a9533756
Ermin <ermin-lol@hotmail.com>
2016-12-06 01:37:29 +0100

    Add files via upload

---
+++ server-nancyfx/Model/Notifications.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SocialNetwork.Model;
+
+namespace SocialNetworkServer.Model
+{
+    public class Notifications
+    {
+        [Key]
+        public int notificationsId { get; set; }
+        public int notificationType { get; set; }
+        public int creatorId { get; set; }
+        public int entityTargetId { get; set; }
+
+
+        //setting up 1:n relation with user
+        public virtual User user { get; set; }
+
+
+    }
+}
