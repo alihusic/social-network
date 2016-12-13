63a61b7dafbecd3710beef8df0e7bf3ca7174603
Ali Husić <ali.husic00@gmail.com>
2016-12-13 01:42:29 +0100

    Model changes

---
+++ server-nancyfx/Model/UserFriendsInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClientSN.Model
+{
+    public class UserFriendsInfo
+    {
+        public int userId { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string pictureURL { get; set; }
+    }
+}
