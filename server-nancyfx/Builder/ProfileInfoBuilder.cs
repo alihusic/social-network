d75ce43fbd709d5430fa981f4b10eb2303afa4fc
Ali Husić <ali.husic00@gmail.com>
2016-12-13 01:41:55 +0100

    Builder changes

---
+++ server-nancyfx/Builder/ProfileInfoBuilder.cs
@@ -0,0 +1,104 @@
+﻿using SocialNetwork.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClientSN.Model;
+
+namespace SocialNetworkServer.Builder
+{
+    class ProfileInfoBuilder
+    {
+        private string name;
+        private string lastName;
+        private string username;
+        private string password;
+        private string country;
+        private string city;
+        private string pictureURL;
+        private string coverPictureURL;
+        private string gender;
+        private DateTime dateOfBirth;
+
+
+        public ProfileInfoBuilder Name(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public ProfileInfoBuilder LastName(string lastName)
+        {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public ProfileInfoBuilder Username(string username)
+        {
+            this.username = username;
+            return this;
+        }
+
+        public ProfileInfoBuilder Country(string country)
+        {
+            this.country = country;
+            return this;
+        }
+
+
+
+        public ProfileInfoBuilder City(string city)
+        {
+            this.city = city;
+            return this;
+        }
+
+
+
+        public ProfileInfoBuilder PictureURL(string pictureURL)
+        {
+            this.pictureURL = pictureURL;
+            return this;
+        }
+
+
+
+        public ProfileInfoBuilder CoverPictureURL(string coverPictureURL)
+        {
+            this.coverPictureURL = coverPictureURL;
+            return this;
+        }
+
+
+
+        public ProfileInfoBuilder Gender(string gender)
+        {
+            this.gender = gender;
+            return this;
+        }
+
+
+        public ProfileInfoBuilder DateOfBirth(DateTime dateOfBirth)
+        {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public ProfileInfo Build()
+        {
+            return new ProfileInfo()
+            {
+                name = name,
+                lastName = lastName,
+                username = username,
+                country = country,
+                city = city,
+                pictureURL = pictureURL,
+                coverPictureURL = coverPictureURL,
+                gender = gender,
+                dateOfBirth = dateOfBirth
+            };
+        }
+    }
+}
