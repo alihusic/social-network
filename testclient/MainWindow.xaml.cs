42745cf2913b418557225199d1c41b7a974a0389
pasictarik <tarikpasic112@gmail.com>
2016-12-14 22:45:35 +0100

    Add files via upload

---
+++ testclient/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void logInButtonClick(object sender, RoutedEventArgs e)
+        {
+            //checks that client makes go here !
+            Main neat = new Main();
+            neat.Show();
+            this.Close();
+        }
+
+        private void createNewAccountButtonClick(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+            this.Close();
+        }
+    }
+}
---
+++ testclient/SignUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Neat
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void logInButtonClick(object sender, RoutedEventArgs e)
+        {
+            LogIn logIn = new LogIn();
+            logIn.Show();
+            this.Close();
+        }
+
+        private void signUpButtonClick(object sender, RoutedEventArgs e)
+        {
+            //checks that client makes go here !
+            Main neat = new Main();
+            neat.Show();
+            this.Close();
+        }
+    }
+}
